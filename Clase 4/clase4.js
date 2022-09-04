ca1ba0a7bb5951f676e97a55f48543c7935754c7
isabelortelli <111462105+isabelortelli@users.noreply.github.com>
2022-09-04 19:14:25 -0300

    Add files via upload

---
+++ Clase 4/clase4.js
@@ -0,0 +1,30 @@
+// distancia determine el medio de transporte apropiado de acuerdo a las siguientes reglas:
+
+//0 a 1000 metros = pie
+//1000 a 10000 metros = bicicleta
+//10000 a 30000 metros = colectivo
+//30000 a 100000 metros = auto
+//+100000 = avión
+
+<body>
+    <script>
+        
+        const distancia = (prompt('Ingrese la distancia:',''))
+       
+
+
+    </script>
+</body>
+
+// if (distancia < 1000) {
+//  document.write('El medio de transporte apropiado es a pie');
+//} else if (distancia > 1000 & distancia< 10000) {
+//    document.write('El medio de transporte apropiado es bicicleta');
+//} else if (distancia>10000 & distancia<30000)
+//    document.write('El medio de transporte apropiado es el colectivo');
+//} else if (distancia>30000 & distancia<100000)
+//    document.write('El medio de transporte apropiado es el auto');
+//} else {
+//    document.write('El medio de transporte apropiado es avion')
+//}
+
